12633f00ef5d6714d335d9d05e643dbb4be6d6bd
Sparidae <2992646478@qq.com>
2024-05-31 17:00:04 +0800

    添加了指标计算和数据处理

--- data_process.py
+++ data_process.py
@@ -1,3 +1,12 @@
 # 对数据进行一些预处理的函数
+from datasets import load_dataset
 
+DATASET_CACHE = "./dataset/hf_dataset"
 
+dataset = load_dataset(
+    "botisan-ai/cantonese-mandarin-translations", cache_dir=DATASET_CACHE
+)
+
+dataset = load_dataset(
+    "raptorkwok/cantonese-traditional-chinese-parallel-corpus", cache_dir=DATASET_CACHE
+)
