24e1b6f4d2339f01a3d03166053e2552e6310a76
Sparidae <2992646478@qq.com>
2024-06-05 11:02:36 +0800

    补充model标准

--- model/model1.py
+++ model/model1.py
@@ -3,6 +3,8 @@ import torch
 import torch.nn as nn
 from transformers import PretrainedConfig, PreTrainedModel
 
+# 以下代码仅示例
+
 
 class BertConfig(PretrainedConfig):
     def __init__(
@@ -12,6 +14,32 @@ class BertConfig(PretrainedConfig):
     ):
         self.param = param
         super().__init__(**kwargs)
+
+
+class BertForNER(PreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)  # 模型需要接受 config 参数并且传递给父类
+        self.bert = Bert(config)
+        self.loss_func = nn.CrossEntropyLoss()
+        self.is_pretrain = config.is_pretrain
+
+    def forward(
+        self,  # 接收参数需要与数据集的键对应
+        x,
+        position,
+        padding_mask,
+        labels,
+    ):
+        pass
+        # 传递给 `Trainer` 的模型必须在模型内部 `.forward()` 计算 loss ，
+        # 作为元组第一个元素返回，或者返回包含 `loss` 键的字典
+        # y = self.bert(x, position, padding_mask, self.is_pretrain)
+        # loss = self.loss_func(y.permute(0, 2, 1), labels)
+        # return loss, y
+        # return {
+        #     "loss": loss,
+        #     "y": y,
+        # }
 
 
 class Bert(PreTrainedModel):
