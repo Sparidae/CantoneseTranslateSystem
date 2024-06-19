ed54acc57c3ed7c0873524bbc9dbec9c4f36d76b
Sparidae <2992646478@qq.com>
2024-06-19 11:12:13 +0800

    修改ui

--- ui.py
+++ ui.py
@@ -1,13 +1,19 @@
 import os
 import shutil
 from datetime import datetime
-from infer_t5 import InferT5
+
 import gradio as gr
 
+from infer_t5 import InferT5
 from interface.audio import speech2text
 from interface.translate import translate_yue_to_zh
+from utils import get_logger
 
-t5_model = InferT5("output/t5_lora/Jun17_14-11-08/checkpoint-9216", mode='lora')
+logger = get_logger("UI")
+
+logger.info("loading model")
+t5_model = InferT5("output/t5_lora/Jun17_14-11-08/checkpoint-9216", mode="lora")
+
 
 # 保存上传的音频文件到指定目录
 def save_audio_file(file_path):
@@ -21,6 +27,7 @@ def save_audio_file(file_path):
     shutil.copy(file_path, new_file_path)
     return new_file_path
 
+
 def translate_text(yue_text, model_name, strategy):
     if model_name == "t5":
         if strategy == "搜索":
@@ -29,19 +36,22 @@ def translate_text(yue_text, model_name, strategy):
         elif strategy == "采样":
             text = t5_model.translate_yue_to_zh(yue_text, do_sample=True)
             text = f"{text[0]}"
+    # elif model_name == 't5_3b':
+
     elif model_name == "lstm":
         pass
     elif model_name == "讯飞API":
         text = translate_yue_to_zh(yue_text)
-    
+
     return text
+
 
 # 主函数，处理音频文件和文本输入
 def process_input(audio_file, mic_file, yue_text_input, model, strategy):
     yue_text = ""
     cn_text = ""
 
-    if model==None or strategy==None:
+    if model is None or strategy is None:
         return (
             "",
             "",
@@ -90,13 +100,23 @@ def process_input(audio_file, mic_file, yue_text_input, model, strategy):
 
     return yue_text, cn_text, history, "<p style='color:green'>操作成功</p>"
 
+
 def update_strategy(model_name):
-    if model_name == "t5":
-        return gr.update(choices=["搜索", "采样"], value="搜索")
-    elif model_name == "lstm":
-        return gr.update(choices=["采样"], value="采样")
-    elif model_name == "讯飞API":
-        return gr.update(choices=["无"], value="无")
+    strategies = {
+        "t5_3b": ["搜索", "采样"],
+        "t5": ["搜索", "采样"],
+        "lstm": ["搜索"],
+        "讯飞API": ["无"],
+    }
+    choices = strategies.get(model_name)
+    return gr.update(choices=choices, value=choices[0])
+    # if model_name == "t5":
+    #     return gr.update(choices=["搜索", "采样"], value="搜索")
+    # elif model_name == "lstm":
+    #     return gr.update(choices=["搜索"], value="搜索")
+    # elif model_name == "讯飞API":
+    #     return gr.update(choices=["无"], value="无")
+
 
 # 初始化历史记录
 history = []
@@ -107,23 +127,29 @@ with gr.Blocks(
 ) as demo:  # slate gray zinc neutral stone red orange amber yellow lime green emerald teal cyan sky blue indigo violet purple fuchsia pink rose
     gr.HTML("""<h1 align="center">粤语-简体中文翻译系统</h1>
             <p align="center">支持语音翻译和文本翻译""")
-    
+
     with gr.Row():
-        model_name = gr.Dropdown(choices=["t5", "lstm", "讯飞API"], label="选择翻译模型")
+        model_name = gr.Dropdown(
+            choices=["t5_3b", "t5", "lstm", "讯飞API"], label="选择翻译模型"
+        )
         strategy = gr.Dropdown(choices=["无"], label="选择生成策略")
 
     # 更新策略选项
     model_name.change(fn=update_strategy, inputs=model_name, outputs=strategy)
 
     # 上传语音文件接口
-    with gr.Row():
-        audio_input = gr.Audio(sources="upload", type="filepath", label="上传语音文件")
-        mic_input = gr.Audio(
-            sources="microphone", type="filepath", label="用麦克风录音"
-        )
+    with gr.Group():
+        # gr.Markdown("## 上传语音文件和输入粤语文本")
+        with gr.Row():
+            audio_input = gr.Audio(
+                sources="upload", type="filepath", label="上传语音文件"
+            )
+            mic_input = gr.Audio(
+                sources="microphone", type="filepath", label="用麦克风录音"
+            )
 
-    # 粤语文本输入接口
-    yue_text_input = gr.Textbox(label="输入粤语文本直接翻译", lines=3)
+        # 粤语文本输入接口
+        yue_text_input = gr.Textbox(label="输入粤语文本直接翻译", lines=3)
 
     # 提示信息弹出框
     alert_output = gr.HTML()
